a5c15ccdcc922d7496f6dd87401eca2ba5dfdb05
Joseph Stewart <JosephStewart1752@gmail.com>
2023-08-02 13:21:23 -0400

    Add Push Method

--- pairing_2/friends.js
+++ pairing_2/friends.js
@@ -2,3 +2,7 @@ var arrays = ["Heather", "Peter", "Kelly", "Roland", "Jason"]
 console.log(arrays[2])
 console.log(arrays[4])
 console.log(arrays[0])
+
+var array2 = ["Brazil", "Jamaica", "Puerto Rico", "Thailand", "Columbia"]
+array2.push("Portugal")
+console.log(array2)
